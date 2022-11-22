5fb4d5d361c96d20857f5b49e2cfada4aa7664b7
dolev146 <dolev146@gmail.com>
2022-11-22 15:44:34 +0200

    fuck why this shit doesnt work

--- app/src/main/java/com/example/firebasecrudapplication/MainActivity.java
+++ app/src/main/java/com/example/firebasecrudapplication/MainActivity.java
@@ -7,17 +7,28 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -31,6 +42,7 @@ public class MainActivity extends AppCompatActivity implements CourseRVAdapter.C
     private ArrayList<CourseRVModal> courseRVModalArrayList;
     private RelativeLayout bottomSheetRL;
     private CourseRVAdapter courseRVAdapter;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,10 @@ public class MainActivity extends AppCompatActivity implements CourseRVAdapter.C
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Courses");
         courseRVModalArrayList = new ArrayList<>();
+        bottomSheetRL = findViewById(R.id.idRLBSheet);
+        mAuth = FirebaseAuth.getInstance();
+
+
         courseRVAdapter = new CourseRVAdapter(courseRVModalArrayList, this, this);
         courseRV.setLayoutManager(new LinearLayoutManager(this));
         courseRV.setAdapter(courseRVAdapter);
@@ -55,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements CourseRVAdapter.C
         });
     }
 
-    private  void getAllCourses(){
+    private void getAllCourses() {
         courseRVModalArrayList.clear();
         databaseReference.addChildEventListener(new ChildEventListener() {
             @Override
@@ -95,5 +111,75 @@ public class MainActivity extends AppCompatActivity implements CourseRVAdapter.C
     public void onCourseClick(int position) {
 
 
+    }
+
+    private void displayBottomSheet(CourseRVModal courseRVModal) {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(this);
+        View layout = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_dialog, bottomSheetRL);
+        bottomSheetDialog.setContentView(layout);
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.setCanceledOnTouchOutside(true);
+
+        bottomSheetDialog.show();
+
+        TextView courseNameTV = layout.findViewById(R.id.idTVCourseName);
+        TextView courseDescTV = layout.findViewById(R.id.idTVDescription);
+        TextView courseSuitedForTV = layout.findViewById(R.id.idTVSuitedFor);
+        TextView coursePriceTV = layout.findViewById(R.id.idTVPrice);
+        ImageView courseIV = layout.findViewById(R.id.idIVCourse);
+        Button editBtn = layout.findViewById(R.id.idBtnEdit);
+        Button viewDetailsBtn = layout.findViewById(R.id.idBtnViewDetails);
+
+
+        courseNameTV.setText(courseRVModal.getCourseName());
+        courseDescTV.setText(courseRVModal.getCourseDesc());
+        courseSuitedForTV.setText(courseRVModal.getBestSuitedFor());
+        coursePriceTV.setText(courseRVModal.getCoursePrice());
+
+        Picasso.get().load(courseRVModal.getCourseImg()).into(courseIV);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, EditCourseActivity.class);
+                i.putExtra("course", courseRVModal);
+                startActivity(i);
+            }
+        });
+
+        viewDetailsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(courseRVModal.getCourseLink()));
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.idLogOut:
+                Toast.makeText(this, "User Logged Out.. ", Toast.LENGTH_SHORT).show();
+                mAuth.signOut();
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
